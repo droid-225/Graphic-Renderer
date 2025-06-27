9cbfa4d87978ef041347e145747c848189f1f7a7
Rishit Shah <aryanrishit1@gmail.com>
2025-06-28 00:24:11 +0530

    Commit 11: Created a bunch of geometric shape drawing methods, tried to make a Christmas tree method, maybe next time.

--- src/StaticGraphics.java
+++ src/StaticGraphics.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class StaticGraphics extends JPanel {
+    final int PIXELSIZE = 1;
     int width, height;
 
     StaticGraphics(int width, int height) {
@@ -39,18 +40,72 @@ public class StaticGraphics extends JPanel {
 
         g.setColor(Color.BLACK);
         //drawSquare(g, 10, 10, 100, 100);
-        drawRainbowSquare(g, 10, 10, 490, 490);
+        //drawRainbowSquare(g, 10, 10, 490, 490);
+        //drawEquilateralTriangle(g, 100, 100, 100);
+        //drawRandomColoredPixels(g, 100);
+        drawTriangle2(g, 100, 100, 50);
     }
 
-    public void drawSquare(Graphics g, int startXPos, int startYPos, int width, int height) {
-        for(int i = 0; i < width; i++) {
-            for(int j = 0; j < height; j++) {
-                g.fillRect(startXPos + i, startYPos + j, 1, 1);
+    private void drawTriangle2(Graphics g, int xPos, int yPos, int height) {
+        int x = 0;
+
+        for(int i = 0; i < height; i++) {
+            for(int j = 1; j <= height - x; j++)
+                g.fillRect(xPos + j + x - height, yPos - i + height, PIXELSIZE, PIXELSIZE);
+            x++;
+        }
+
+        for(int i = 0; i <= height; i++) {
+            for(int j = 0; j <= i; j++)
+                g.fillRect(xPos + j, yPos, PIXELSIZE, PIXELSIZE);
+
+            yPos++;
+        }
+    }
+
+    private void drawRandomColoredPixels(Graphics g, int numOfPixels) {
+        int xPos, yPos, r, gr, b;
+
+        for(int i = 0; i < numOfPixels; i++) {
+            xPos = (int)(Math.random() * (width - 10));
+            yPos = (int)(Math.random() * (height - 10));
+            r = (int)(Math.random() * 255);
+            gr = (int)(Math.random() * 255);
+            b = (int)(Math.random() * 255);
+
+            g.setColor(new Color(r, gr, b));
+            g.fillRect(xPos, yPos, PIXELSIZE, PIXELSIZE);
+        }
+    }
+
+    private void drawRandomPixels(Graphics g, int numOfPixels) {
+        int xPos;
+        int yPos;
+
+        for(int i = 0; i < numOfPixels; i++) {
+            xPos = (int)(Math.random() * (width - 10));
+            yPos = (int)(Math.random() * (height - 10));
+            g.fillRect(xPos, yPos, PIXELSIZE, PIXELSIZE);
+        }
+    }
+
+    private void drawEquilateralTriangle(Graphics g, int xPos, int yPos, int height) {
+        for(int j = 0; j < height; j++) {
+            for(int i = 0; i < height - j; i++) {
+                g.fillRect(xPos + j, yPos - i, PIXELSIZE, PIXELSIZE);
             }
         }
     }
 
-    public void drawRainbowSquare(Graphics g, int startXPos, int startYPos, int width, int height) {
+    private void drawSquare(Graphics g, int startXPos, int startYPos, int width, int height) {
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                g.fillRect(startXPos + i, startYPos + j, PIXELSIZE, PIXELSIZE);
+            }
+        }
+    }
+
+    private void drawRainbowSquare(Graphics g, int startXPos, int startYPos, int width, int height) {
         int r;
         int gr;
         int b;
@@ -62,7 +117,22 @@ public class StaticGraphics extends JPanel {
 
             for(int j = 0; j < height; j++) {
                 g.setColor(new Color(r, gr, b));
-                g.fillRect(startXPos + i, startYPos + j, 1, 1);
+                g.fillRect(startXPos + i, startYPos + j, PIXELSIZE, PIXELSIZE);
+            }
+        }
+    }
+
+    private void drawWeirdLine(Graphics g, int startingXPos, int startingYPos, int height) {
+        int x = 0;
+
+        for(int i = 0; i < height; i++) {
+            x = i;
+
+            if(i > 0) {
+                while(x != 0) {
+                    g.fillRect(startingXPos + i, startingYPos + i, PIXELSIZE, PIXELSIZE);
+                    x--;
+                }
             }
         }
     }
